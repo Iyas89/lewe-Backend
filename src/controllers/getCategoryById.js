3bfd1e0b705c44c05e3f685648bd917cf680eb88
FOteroRimini <facurimini@gmail.com>
2024-02-29 17:56:55 -0300

    category name

--- src/controllers/getCategoryById.js
+++ src/controllers/getCategoryById.js
@@ -1,17 +1,23 @@
 const { Articles } = require("../db");
+const { Category } = require("../db");
 
 const getCategoryById = async (id) => {
   try {
     if (id) {
+      const categoryName = await Category.findByPk(id);
       const categoryProducts = await Articles.findAll({
         where: {
           CategoryId:  id ,
         },
       });
+      const name = categoryName.dataValues.name;
+
         if (categoryProducts === null) {
             throw new Error("No se encontraron productos en esta categoría");
         }
-      return categoryProducts;
+
+     return {products: categoryProducts, name: name};
+     
     }
   } catch (error) {
     throw new Error(error.message);
--- src/handlers/getCategoryByIdHandler.js
+++ src/handlers/getCategoryByIdHandler.js
@@ -2,9 +2,10 @@ const { getCategoryById } = require('../controllers/getCategoryById');
 
 const getCategoryByIdHandler = async (req, res) => {
     const { id } = req.params;
+    console.log(id);
     try {
-        const categoryProducts = await getCategoryById(id);
-        return res.status(200).json({categoryProducts});
+        const categoryDetails = await getCategoryById(id);
+        return res.status(200).json(categoryDetails);
     } catch (error) {
         return res.status(400).json({ message: error.message });
     }
