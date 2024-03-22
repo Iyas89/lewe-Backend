bfc6f5389061944466fed6dc11b1b1ebec6ec748
FOteroRimini <facurimini@gmail.com>
2024-03-22 16:34:59 -0300

    articles name

--- src/controllers/getOrders.js
+++ src/controllers/getOrders.js
@@ -1,7 +1,9 @@
 const {OrderArticles} = require("../db");
+const {Articles} = require("../db");
 
 const getOrders = async () => {
     const orders = await OrderArticles.findAll();
+    const articles = await Articles.findAll();
 
     const groupedOrders = {};
     orders.forEach(order => {
@@ -9,14 +11,16 @@ const getOrders = async () => {
         groupedOrders[order.OrderId] = [];
       }
       groupedOrders[order.OrderId].push({
-        ArticleId: order.ArticleId,
+        ArticleId: order.ArticleId.slice(-6),
+        name: articles.find(article => article.id === order.ArticleId).name,
+        image: articles.find(article => article.id === order.ArticleId).image,
         quantity: order.quantity,
         size: order.size
       });
     });
 
     const groupedOrdersArray = Object.keys(groupedOrders).map(orderId => ({
-      OrderId: orderId,
+      OrderId: orderId.slice(-6),
       articles: groupedOrders[orderId]
     }));
 
